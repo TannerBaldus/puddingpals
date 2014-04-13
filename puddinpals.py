f3bd2b65bb3cc67139a1784b2c2618163fe9d773
Tanner Baldus <tanner@irontop.(none)>
2014-04-12 17:30:19 -0700

    got add remove and sort working

--- puddinpals.py
+++ puddinpals.py
@@ -1,44 +1,53 @@
 import csv
 import unittest
+import docopt
 
 
 class Contact(object):
-	def __init__(self,firstname,lastname,phone,address,zipcode):
-		self.firstname = firstname
-		self.lastname = lastname
+	def __init__(self,name,phone,address,zipcode):
+		self.name = name
 		self.phone = phone
 		self.zipcode = zipcode
 		self.address = address
+		self.attrs = {'name':self.name,'phone':self.phone,'zipcode':self.zipcode}
 
 	def getAttr(self,attr):
 		"""We can use this to sort the contacts by attribute """
-
-		if attr=='firstname': return self.firstname
-		elif attr=='lastname': return self.lastname
-		elif attr=='phone': return self.phone
-		elif attr=='zipcode': return self.zipcode
-		elif attr=='address':return self.address
-
+		return self.attrs[attr]
 
 class AddressBook(object):
 
 	def __init__(self,csvfile=''):
 		self.contacts = []
+		# csvfile = open(csvfile,'rb')
+		# self.reader=reader(csvfile)
 
-	def sort(self,attr):
+
+	def sort(self,attr,isDescending):
 		contactAttr = lambda contact: contact.getAttr(attr)
-		self.contacts.sort(key=contactAttr)
+		self.contacts.sort(key=contactAttr,reverse=isDescending)
 
-	def addContact(self,firstname='',lastname='',phone='',address='',zipcode=''):
-		self.contacts.append(Contact(firstname,lastname,phone,address,zipcode))
+	def addContact(self,name='',phone='',address='',zipcode=''):
+		self.contacts.append(Contact(name, phone, address, zipcode))
+
+	def removeContact(self,selected):
+		removal = [self.contacts.remove(contact) for contact in selected]
+
+
+	def readTSV(self,filepath):
+		tsv = open(filepath,'r')
+		reader = csv.reader=(tsv,delimiter='\t')
+
+
 
 class ABTest(unittest.TestCase):
 
 	def testAdd(self):
 		ab = AddressBook()
-		ab.addContact('Tester','Mcgee','985.555.555','555 foo lane','70471')
-		self.assertEqual( ab.contacts[0].firstname,'Tester')
+		ab.addContact('Tester Mcgee','985.555.555','555 foo lane','70471')
+		self.assertEqual( ab.contacts[0].name,'Tester Mcgee')
 
+		
 	def testSort(self):
 		ab = AddressBook()
 		names = ['Adam','Bane','Charles']
@@ -46,9 +55,21 @@ class ABTest(unittest.TestCase):
 		ab.addContact(names[1])
 		ab.addContact(names[0])
 
-		ab.sort('firstname')
-		abNames = [contact.firstname for contact in ab.contacts]
+		ab.sort('name',False)
+		abNames = [contact.name for contact in ab.contacts]
 		self.assertEqual(names,abNames)
+
+	def testRemove(self):
+		ab = AddressBook()
+		names = ['Adam','Bane','Charles']
+		ab.addContact(names[0])
+		ab.addContact(names[1])
+		ab.addContact(names[2])
+		selected = [ab.contacts[0]]
+		ab.removeContact(selected)
+		abNames = [contact.name for contact in ab.contacts]
+		self.assertEqual(names[1:],abNames)
+
 
 
 
@@ -56,6 +77,7 @@ class ABTest(unittest.TestCase):
 
 if __name__ == '__main__':
 	unittest.main()
+
 
 
 
