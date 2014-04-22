e265e41d6ee5b8678b2a43df3968698a648fe7b8
Tanner Baldus <tanner@irontop.(none)>
2014-04-21 22:31:03 -0700

    refactored validator test

--- ValidatorTest.py
+++ ValidatorTest.py
@@ -9,34 +9,41 @@ class ValidatorTest(unittest.TestCase):
 		self.assertEqual(good,v.sanitize(bad))
 
 	def testGoodZip(self):
+		v = Validator()
 		zipcode = '97401'
-		self.assertTrue(Validator.isValidZip(zipcode))
+		self.assertTrue(v.isValidZip(zipcode))
 
 	def testBadZip(self):
+		v = Validator()
 		zipcode = '7401'
-		self.assertFalse(Validator.isValidZip(zipcode))
+		self.assertFalse(v.isValidZip(zipcode))
 
 	def testGoodLongZip(self):
+		v = Validator()
 		zipcode = '97401-1234'
-		self.assertTrue(Validator.isValidZip(zipcode))
+		self.assertTrue(v.isValidZip(zipcode))
 
 	def testBadLongZip(self):
+		v = Validator()
 		zipcode = '97-4011282'
-		self.assertFalse(Validator.isValidZip(zipcode))
+		self.assertFalse(v.isValidZip(zipcode))
 
 	def testGoodPhone(self):
+		v = Validator()
 		phone = '985.377.9835'
-		self.assertTrue(Validator.isValidPhone(phone))
+		self.assertTrue(v.isValidPhone(phone))
 
 	def  testDNETSV(self):
-		"""tests if validator rejects files that don't exist"""
+		v = Validator()
+		"""tests if v rejects files that don't exist"""
 		filein = 'blah.tsv'
-		self.assertFalse(Validator.isValidUSPS(filein))
+		self.assertFalse(v.isValidUSPS(filein))
 
 		
 	def testNonTSV(self):
+		v = Validator()
 		filein = 'testTSV/test.csv'
-		self.assertFalse(Validator.isValidUSPS(filein))
+		self.assertFalse(v.isValidUSPS(filein))
 		
 	
 
