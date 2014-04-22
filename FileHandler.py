51b45204ddf9e93924503b3de1cb5139fa8441cc
Tanner Baldus <tanner@irontop.(none)>
2014-04-22 14:29:29 -0700

    tenative fix of filehandler

--- FileHandler.py
+++ FileHandler.py
@@ -4,69 +4,71 @@ from Validator import *
 
 class FileHandler(object):
 
-	def __init__(self):
+    def __init__(self):
          self.Validator = Validator()
          self.uspsFields = ['Last','Delivery','Second','Recipient','Phone']
 
 
-	def ValidAttr(self, attrName, attr):
-		""" Returns Empty String if Invalid, else returns sanitized attr"""
-		if self.Validator.validateAttr(attrName, attr) :
-			return self.Validator.sanitize(attr)
+    def ValidAttr(self, attrName, attr):
+        """ Returns Empty String if Invalid, else returns sanitized attr"""
+        if self.Validator.validateAttr(attrName, attr) :
+            return self.Validator.sanitize(attr)
 
-		return ''
+        return ''
 
-	def splitLast(self,last):
-		row = last.split(' ')
-		stateIndex = None
-		for index, el in enumerate(row):
-			# print el
-			if self.Validator.isValidState(el):
+    def splitLast(self,last):
+        row = last.split(' ')
+        if len(row)>3:
+            stateIndex = None
+            for index, el in enumerate(row):
+                # print el
+                if self.Validator.isValidState(el):
+                    stateIndex = index
 
-				stateIndex = index
+            if stateIndex:
+                city = ' '.join(row[0:stateIndex])
+                state = row[stateIndex]
+                zipcode = row[stateIndex+1]
+                return [city,state,zipcode]
 
-		if stateIndex:
-			city = ' '.join(row[0:stateIndex])
-			state = row[stateIndex]
-			zipcode = row[stateIndex+1]
-			return [city,state,zipcode]
-
-		else:
-			return ['','','']
+        return ['','','']
 
 
-	def readUSPS(self,filepath):
-		tsv = open(filepath,'r')
-		reader = csv.DictReader(tsv, delimiter='\t',restval='')
-		attributes =[]
+    def readUSPS(self,filepath):
+        tsv = open(filepath,'r')
+        reader = csv.DictReader(tsv, delimiter='\t',restval='')
+        attributes =[]
 
-		for row in reader:
-			attr ={} 
-			# print row
-			last = self.splitLast(row['Last'])
-			attr['name'] = row['Recipient']
-			attr['phone'] = self.ValidAttr('phone',row['Phone'])
-			attr['address'] = row['Delivery']
-			attr['address2'] = row['Second']
-			attr['city'] = self.ValidAttr('city',last[0])
-			attr['state'] = self.ValidAttr('state',last[1])
-			attr['zipcode'] = self.ValidAttr('zipcode',last[2])
-			attributes.append(attr)
-		return attributes
-
+        for row in reader:
+            attr ={} 
+            # print row
+            last = self.splitLast(row['Last'])
+            attr['name'] = row['Recipient']
+            attr['phone'] = self.ValidAttr('phone',row['Phone'])
+            attr['address'] = row['Delivery']
+            attr['address2'] = row['Second']
+            attr['city'] = self.ValidAttr('city',last[0])
+            attr['state'] = self.ValidAttr('state',last[1])
+            attr['zipcode'] = self.ValidAttr('zipcode',last[2])
+            attributes.append(attr)
+        return attributes
 
 
 
-	def writeUSPS(self,contacts, filepath):
-		tsv = open(filepath,'w')
-		writer = csv.DictWriter(tsv, delimiter='\t', fieldnames=self.uspsFields)
-		writer.writeheader()
-		for contact in contacts:
-			attributes ={}
-			attributes['Recipient'] = contact.getAttr('name')
-			last = [contact.getAttr('city'),contact.getAttr('state'),contact.getAttr('zipcode')]
-			attributes['Second'] = contact.getAttr('address2')
-			attributes['Last'] = ' '.join(last)
-			attributes['Delivery'] = contact.getAttr('address2')
-			attributes['Phone'] = contact.getAttr('phone')
-			writer.writerow(attributes)
+
+    def writeUSPS(self,contacts, filepath):
+        tsv = open(filepath,'w')
+        writer = csv.DictWriter(tsv, delimiter='\t', fieldnames=self.uspsFields)
+        writer.writeheader()
+        for contact in contacts:
+            attributes ={}
+            attributes['Recipient'] = contact.getAttr('name')
+            last = [contact.getAttr('city'),contact.getAttr('state'),contact.getAttr('zipcode')]
+            attributes['Second'] = contact.getAttr('address2')
+            attributes['Last'] = ' '.join(last)
+            attributes['Delivery'] = contact.getAttr('address2')
+            attributes['Phone'] = contact.getAttr('phone')
+            writer.writerow(attributes)
+if __name__ == '__main__':
+    f = FileHandler()
+    f.readUSPS('testTSV/testero.tsv')
